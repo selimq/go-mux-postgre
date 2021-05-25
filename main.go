da0182fd9749dee515723fdc0dac12c497027f26
Selim Doğan <sselimq@gmail.com>
2021-05-26 01:38:31 +0300

    .,

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
-import "os"
+import "github.com/selimq/go-mux-postgre/app"
 
 func main() {
-	a := App{}
+	a := app.App{}
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		"postgres", "", "postgres")
 
 	a.Run(":8010")
 }
--- main_test.go
+++ main_test.go
@@ -1,9 +1,65 @@
-package main_test
+package main
 
-import "testing"
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
 
-//var a main.App
+	"github.com/selimq/go-mux-postgre/app"
+)
+
+var a = app.App{}
 
 func TestMain(m *testing.M) {
+	a.Initialize(
+		"postgres", "SamplePass", "postgres")
+	ensureTableExists()
+	code := m.Run()
+	clearTable()
+	os.Exit(code)
+}
 
+func ensureTableExists() {
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func clearTable() {
+	a.DB.Exec("DELETE FROM products")
+	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+}
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
+(
+    id SERIAL,
+    name TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+)`
+
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/products", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
+	}
+}
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
 }
